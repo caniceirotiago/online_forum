fbcd5442ab75422cfd7764d1b4effe696c4eacdb
Tiago Caniceiro <tiagocaniceiro@cron.studio>
2024-08-18 22:08:31 +0100

    We already have topic view completed but we have to make login and authentication first

--- forum/urls.py
+++ forum/urls.py
@@ -2,12 +2,20 @@ from django.urls import path
 from .views import views_forum, views_topic, views_post
 
 urlpatterns = [
+    # Forums
     path('forums/', views_forum.forum_list, name='forum_list'),
     path('forum/', views_forum.forum_create, name='forum_create'),
     path('forum/<int:pk>/', views_forum.forum_detail, name='forum_detail'),
     path('forum/<int:pk>/update/', views_forum.forum_update, name='forum_update'),
     path('forum/<int:pk>/delete/', views_forum.forum_delete, name='forum_delete'),
+
+    # Topics
     path('forum/<int:pk>/new_topic/', views_topic.new_topic, name='new_topic'),
     path('topic/<int:pk>/', views_topic.topic_detail, name='topic_detail'),
+    path('topics/<int:pk>/', views_topic.topic_list_by_forum, name='topic_list_by_forum'),
+    path('topic/<int:pk>/update/', views_topic.topic_update, name='topic_update'),
+    path('topic/<int:pk>/delete/', views_topic.topic_delete, name='topic_delete'),
+
+    # Posts
     path('topic/<int:pk>/new_post/', views_post.new_post, name='new_post'),
 ]
--- forum/views/views_topic.py
+++ forum/views/views_topic.py
@@ -1,32 +1,47 @@
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from forum.models import Forum, Topic, Post
-from forum.forms import NewTopicForm, NewPostForm
-from django.views.decorators.http import require_http_methods
+from rest_framework.decorators import api_view
+from forum.models import Forum, Topic
+from forum.forms import NewTopicForm
 
-
-@require_http_methods(["GET"])
-def topic_detail(request, pk):
-    topic = get_object_or_404(Topic, pk=pk)
-    posts = topic.posts.all()
-    posts_data = list(posts.values('id', 'message', 'created_by_id', 'created_at'))
-    topic_data = {
-        'id': topic.id,
-        'subject': topic.subject,
-        'created_by_id': topic.created_by_id,
-        'created_at': topic.created_at,
-        'posts': posts_data,
-    }
-    return JsonResponse({'topic': topic_data})
-
-@require_http_methods(["POST"])
+@api_view(['POST'])
 def new_topic(request, pk):
     forum = get_object_or_404(Forum, pk=pk)
-    form = NewTopicForm(request.POST)
+    form = NewTopicForm(request.data)
     if form.is_valid():
         topic = form.save(commit=False)
         topic.forum = forum
-        topic.created_by = request.user  # Supondo que o usuário esteja logado
+        topic.created_by = request.user
         topic.save()
-        return JsonResponse({'success': True, 'topic_id': topic.id})
-    return JsonResponse({'success': False, 'errors': form.errors}, status=400)
+        return JsonResponse({'topic': topic.id}, status=201)
+    return JsonResponse({'errors': form.errors}, status=400)
+
+@api_view(['Get'])
+def topic_list_by_forum(request, pk):
+    try:
+        topics = Topic.objects.filter(forum=pk)
+        return JsonResponse({'topics': topics})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+@api_view(['GET'])
+def topic_detail(request, pk):
+    topic = get_object_or_404(Topic, pk=pk)
+    return JsonResponse({'topic': topic})
+
+@api_view(['PUT'])
+def topic_update(request, pk):
+    topic = get_object_or_404(Topic, pk=pk)
+    form = NewTopicForm(request.data, instance=topic)
+    if form.is_valid():
+        form.save()
+        return JsonResponse({'topic': topic.id})
+    return JsonResponse({'errors': form.errors}, status=400)
+
+@api_view(['DELETE'])
+def topic_delete(request, pk):
+    topic = get_object_or_404(Topic, pk=pk)
+    topic.delete()
+    return JsonResponse({'message': 'Topic deleted successfully'}, status=204)
+
+
