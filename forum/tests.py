cd62375eb6501d9d36524d0c670953d05c5efce8
Tiago Caniceiro <tiagocaniceiro@cron.studio>
2024-08-21 15:12:55 +0100

    adição de testes

--- forum/tests.py
+++ forum/tests.py
@@ -1,72 +1,42 @@
-from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
 from django.urls import reverse
-from rest_framework.test import APIClient
 from rest_framework import status
+from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 from forum.models import Forum, Post, Topic
 
-class ForumAPITest(TestCase):
+class ForumAPITestCase(APITestCase):
     def setUp(self):
-        self.client = APIClient()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client.force_authenticate(user=self.user)
-        self.forum = Forum.objects.create(name="Test Forum", description="Test Description")
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-    def test_forum_list(self):
-        response = self.client.get(reverse('forum-list'))  # 'forum-list' should be the name of the URL for the ForumAPI list view
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+    # POSITIVE test case - create a forum
+    def test_forum_create(self):
+        self.forum_data = {'name': 'Test Forum', 'description': 'Test Description'}
+        response = self.client.post(reverse('forums-list'), self.forum_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], 'Test Forum')
+        self.assertEqual(response.data['description'], 'Test Description')
+        self.assertIn('id', response.data)
+        self.assertIsInstance(response.data['id'], int)
 
-    def test_forum_retrieve(self):
-        response = self.client.get(reverse('forum-detail', kwargs={'pk': self.forum.id}))  # 'forum-detail' should be the name of the URL for the ForumAPI detail view
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['name'], self.forum.name)
 
-class PostAPITest(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client.force_authenticate(user=self.user)
-        self.forum = Forum.objects.create(name="Test Forum", description="Test Description")
-        self.post = Post.objects.create(forum=self.forum, title="Test Post", content="Test Content", created_by=self.user)
+    # NEGATIVE test case - create a forum (no name)
+    def test_forum_create_bad_request(self):
+        self.forum_data = {'description': 'Test Description'}
+        response = self.client.post(reverse('forums-list'), self.forum_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_post_list(self):
-        response = self.client.get(reverse('post-list'))  # 'post-list' should be the name of the URL for the PostAPI list view
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+    # NEGATIVE test case - create a forum (more than 100 characters on name)
+    def test_forum_create_bad_request_length(self):
+        string_with_100_char = "xTest ForumTest ForumTest ForumTest ForumTest ForumTest ForumTest ForumTest ForumTest ForumTest Forum"
+        self.forum_data = {'name': string_with_100_char, 'description': 'Test Description'}
+        response = self.client.post(reverse('forums-list'), self.forum_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, msg="Expected a 400 Bad Request when name is too long")
 
-    def test_post_retrieve(self):
-        response = self.client.get(reverse('post-detail', kwargs={'pk': self.post.id}))  # 'post-detail' should be the name of the URL for the PostAPI detail view
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], self.post.title)
-
-class TopicAPITest(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client.force_authenticate(user=self.user)
-        self.forum = Forum.objects.create(name="Test Forum", description="Test Description")
-        self.topic = Topic.objects.create(forum=self.forum, title="Test Topic", content="Test Content", created_by=self.user)
-
-    def test_topic_list(self):
-        response = self.client.get(reverse('topic-list'))  # 'topic-list' should be the name of the URL for the TopicAPI list view
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-
-    def test_topic_list_filter_by_forum(self):
-        response = self.client.get(reverse('topic-list'), {'forum_id': self.forum.id})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-
-    def test_topic_list_filter_by_user(self):
-        response = self.client.get(reverse('topic-list'), {'user_id': self.user.id})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-
-    def test_topic_retrieve(self):
-        response = self.client.get(reverse('topic-detail', kwargs={'pk': self.topic.id}))  # 'topic-detail' should be the name of the URL for the TopicAPI detail view
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], self.topic.title)
+    # NEGATIVE test case - create a forum (empty name)
+    def test_forum_create_bad_request_empty_name(self):
+        self.forum_data = {'name': '', 'description': 'Test Description'}
+        response = self.client.post(reverse('forums-list'), self.forum_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, msg="Expected a 400 Bad Request when name is missing")
